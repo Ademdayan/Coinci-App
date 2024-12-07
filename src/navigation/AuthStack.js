d4f6597bde6fcaee292efd1b1b5ddc78fc877e57
Adem Dayan <ademdayan36@gmail.com>
2024-12-07 19:38:54 +0300

    Uygulamanın büyük çogunluğu bitti. screenler oluşturuldu,navigation yapısı tamamlandı,firebase kısmında biraz eksikler kaldı gerisi tamamlandı

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,10 +1,6 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {
-  BottomTabBar,
-  createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import LoginScreen from '../screens/Auth/LoginScreen';
 import SignUpScreen from '../screens/Auth/SignUpScreen';
@@ -17,17 +13,18 @@ const BottomTabStack = () => {
   return (
     <Tab.Navigator
       initialRouteName="LoginScreen"
-      tabBarOptions={{
-        activeTintColor: '#e91e63',
-        inactiveTintColor: '#333',
-        style: {
+      screenOptions={{
+        headerShown: false, // Tüm Tab ekranlarında başlık gizleniyor
+        tabBarActiveTintColor: '#f00',
+        tabBarInactiveTintColor: '#fff',
+        tabBarStyle: {
           height: 70,
-          backgroundColor: '#aaa',
+          backgroundColor: '#000',
           padding: 10,
         },
-        labelStyle: {
-          fontSize: 18,
+        tabBarLabelStyle: {
           textAlign: 'center',
+          fontSize: 18,
         },
       }}>
       <Tab.Screen
@@ -36,42 +33,44 @@ const BottomTabStack = () => {
         options={{
           tabBarLabel: 'Giriş',
           tabBarIcon: ({color, size}) => (
-            <Icon name="user-shield" color={color} size={size}></Icon>
+            <Icon name="user-shield" color={color} size={size} />
           ),
         }}
       />
+
       <Tab.Screen
         name="SignUpScreen"
         component={SignUpScreen}
         options={{
-          tabBarLabel: 'Kayıt Ol',
+          tabBarLabel: 'Yeni Üyelik',
           tabBarIcon: ({color, size}) => (
-            <Icon name="user-plus" color={color} size={size}></Icon>
+            <Icon name="user-plus" color={color} size={size} />
           ),
         }}
       />
+
       <Tab.Screen
         name="ResetPasswordScreen"
         component={ResetPasswordScreen}
         options={{
-          tabBarLabel: 'Şifremi Unuttum',
+          tabBarLabel: 'Şifre Sıfırla',
           tabBarIcon: ({color, size}) => (
-            <Icon name="key" color={color} size={size}></Icon>
+            <Icon name="key" color={color} size={size} />
           ),
         }}
       />
     </Tab.Navigator>
   );
-}
+};
 
 const AuthStack = () => {
   return (
-    <Stack.Navigator initialRouteName="BottomTabStack">
-      <Stack.Screen
-        name="BottomTabStack"
-        component={BottomTabStack}
-        options={{headerShown: false}}
-      />
+    <Stack.Navigator
+      initialRouteName="BottomTabStack"
+      screenOptions={{
+        headerShown: false, // Tüm Stack ekranlarında başlık gizleniyor
+      }}>
+      <Stack.Screen name="BottomTabStack" component={BottomTabStack} />
     </Stack.Navigator>
   );
 };
--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,8 +1,8 @@
-import { View, Text } from 'react-native'
-import React,{useState,useEffect,useContext} from 'react'
-import auth, { onAuthStateChanged } from '@react-native-firebase/auth';
-import { AuthContext } from './AuthProvider';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useState, useEffect, useContext} from 'react';
+import {View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {AuthContext} from './AuthProvider';
+import {NavigationContainer} from '@react-navigation/native';
 import HomeStack from './HomeStack';
 import AuthStack from './AuthStack';
 import Loading from '../utils/Loading';
@@ -10,28 +10,27 @@ import Loading from '../utils/Loading';
 const Routes = () => {
   const {user, setUser} = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
-  const [isInitial, setIsInitial] = useState(true)
-  function onAuthStateChanged(user)  {
+  const [isInitial, setIsInitial] = useState(true);
+
+  function onAuthStateChanged(user) {
     setUser(user);
-    if(isInitial) setIsInitial(false);
+    if (isInitial) setIsInitial(false);
     setIsLoading(false);
   }
-  
   useEffect(() => {
-
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
   }, []);
-  
-  if (isLoading) { 
-    return <Loading />
-    }
-  
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      { user ? <HomeStack/> : <AuthStack/>}
+      {user && user.emailVerified ? <HomeStack /> : <AuthStack />}
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Routes
+export default Routes;
